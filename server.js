3517df7df8e9b945130880860ecf8cd308d97e5b
Yuganish K <yuganish03@gmail.com>
2025-05-08 10:46:48 +0530

    Add project files

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+// Middleware setup
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'yuganish03@gmail.com',
+    pass: 'ngfvrahfwrrwlvxq',
+  },
+});
+
+// POST endpoint to send the email
+app.post('/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: 'yuganish03@gmail.com',
+    subject: `New message from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send('Error sending email');
+    } else {
+      return res.status(200).send('Email sent successfully');
+    }
+  });
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
